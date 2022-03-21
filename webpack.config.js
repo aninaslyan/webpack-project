3284338b33125a0fa57a8b9ea3a0dd863c68d23f
Ani Naslyan <ani.naslyan@gmail.com>
2022-03-21 13:37:19 +0400

    HTMLWebpackPlugin, devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
     mode: 'production',
@@ -6,7 +7,17 @@ const config = {
     entry: './index.js',
     output: {
         filename: "index.bundle.js"
-    }
+    },
+    devServer: {
+        historyApiFallback: true,
+        port: 4200,
+        open: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./index.html",
+        }),
+    ]
 };
 
 module.exports = config;
